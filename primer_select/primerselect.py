68c99b342c10cd01d0948b84ea66ebac213a3804
ssass <steffen.sass@gmx.de>
2014-10-15 15:56:19 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -9,7 +9,7 @@ from Bio import SeqIO
 handle = open(args.input, "rU")
 for record in SeqIO.parse(handle, "fasta"):
     f = open(record.id, 'w')
-    f.write("SEQUENCE_ID=", record.id, "\n")
-    f.write("SEQUENCE_TEMPLATE=", record.seq, "\n")
-    f.write("P3_FILE_FLAG=1\n=")
+    f.write(format("SEQUENCE_ID=", record.id, "\n"))
+    f.write(format("SEQUENCE_TEMPLATE=", record.seq, "\n"))
+    f.write(format("P3_FILE_FLAG=1\n="))
 handle.close()
