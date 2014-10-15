5eb68a6e7931530a16d54b3c6003d5cb08fc4e25
ssass <steffen.sass@gmx.de>
2014-10-15 13:59:11 +0200

    minor changes

--- bartender.py
+++ bartender.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python
-
 import sys
 
-print "Number of arguments:", len(sys.argv), 'arguments.'
-print "Argument List:", str(sys.argv)
+print('Number of arguments:', len(sys.argv), 'arguments.')
+print("Argument List:", str(sys.argv))
