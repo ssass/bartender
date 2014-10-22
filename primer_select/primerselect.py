bc943890d599cd4711467cd481222ee26002f5a4
ssass <steffen.sass@gmx.de>
2014-10-22 13:15:58 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -15,18 +15,34 @@ config = ch.read_config()
 from Bio import SeqIO
 handle = open(args.input, "rU")
 for record in SeqIO.parse(handle, "fasta"):
+    sequence = str(record.seq)
     p3file = record.id + "_seq.txt"
     f = open(p3file, 'w')
     f.write("SEQUENCE_ID=" + record.id + "\n")
-    f.write("SEQUENCE_TEMPLATE=" + str(record.seq) + "\n")
+    f.write("SEQUENCE_TEMPLATE=" + sequence + "\n")
+
+    target_start = sequence.find("\[")
+    target_end = sequence.find("\]")
+    target_length = target_end - target_start - 1
+    print target_start + "\n"
+    print target_length
+
+    print "SEQUENCE_TARGET=" + target_start + "," + target_length
+
+  # exclude.start=regexpr("<",act.seq.ann,fixed=T)[1]
+  # exclude.length=regexpr(">",act.seq.ann,fixed=T)[1]-exclude.start-1
+  # if (exclude.start >(-1) & exclude.length > (-1))write(paste("SEQUENCE_EXCLUDED_REGION=",exclude.start,",",exclude.length,sep=""),file=paste("../p3_input/seq",i,".txt",sep=""),append=T)
+  #
+
+
     f.write("P3_FILE_FLAG=1\n")
     f.write("PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n=")
 
     #print cmd
-    cmd = config.p3_path + " -format_output -p3_settings_file=" + config.p3_config_path+ " -output=/home/icb/steffen.sass/bartender/" + record.id + "_p3.txt /home/icb/steffen.sass/bartender/"+ p3file
+    cmd = config.p3_path + " -format_output -p3_settings_file=" + config.p3_config_path + " -output=" + record.id + "_p3.txt "+ p3file
     print cmd
     args = shlex.split(cmd)
-    p = subprocess.call(args)
+    p = subprocess.Popen(args)
     #p.wait()
 handle.close()
 
