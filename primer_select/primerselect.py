9308f1a1eefb542924298696f20bd377170933a2
ssass <steffen.sass@gmx.de>
2014-10-22 14:01:26 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -19,34 +19,37 @@ handle = open(args.input, "rU")
 for record in SeqIO.parse(handle, "fasta"):
     sequence = str(record.seq)
     p3file = record.id + "_seq.txt"
-    f = open(p3file, 'w')
-    f.write("SEQUENCE_ID=" + record.id + "\n")
-    f.write("SEQUENCE_TEMPLATE=" + sequence + "\n")
+
+    input_String = ""
+    #f = open(p3file, 'w')
+    input_String += "SEQUENCE_ID=" + record.id + "\n"
+    input_String += "SEQUENCE_TEMPLATE=" + sequence + "\n"
 
     target_start = sequence.find("\[")
     target_end = sequence.find("\]")
     target_length = target_end - target_start - 1
 
     if target_start >= 0 & target_length >= 0:
-        f.write("SEQUENCE_TARGET=" + str(target_start) + "," + str(target_length) + "\n")
+        input_String += "SEQUENCE_TARGET=" + str(target_start) + "," + str(target_length) + "\n"
 
     exclude_start = sequence.find("<")
     exclude_end = sequence.find(">")
     exclude_length = exclude_end - exclude_start - 1
 
     if target_start >= 0 & target_length >= 0:
-        f.write("SEQUENCE_EXCLUDED_REGION=" + str(exclude_start) + "," + str(exclude_length) + "\n")
+        input_String += "SEQUENCE_EXCLUDED_REGION=" + str(exclude_start) + "," + str(exclude_length) + "\n"
 
-    f.write("P3_FILE_FLAG=1\n")
-    f.write("PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n=")
+    input_String += "P3_FILE_FLAG=1\n"
+    input_String += "PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n="
 
     # print cmd
     cmd = config.p3_path + " -format_output -p3_settings_file=" + config.p3_config_path + " -output=" + record.id + "_p3.txt"
     print cmd
     args = shlex.split(cmd)
     # print args
-    p = subprocess.call(args)
-    p.communicate(p3file)
+    p = subprocess.Popen(args)
+    p.communicate(input_String)
+    p.terminate()
     #time.sleep(10)
 handle.close()
 #time.sleep(5)
