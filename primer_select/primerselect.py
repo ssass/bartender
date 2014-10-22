658309c6965d6a56a8d860f2ab60cfd5a5aa9fb6
ssass <steffen.sass@gmx.de>
2014-10-22 12:29:29 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -1,4 +1,5 @@
 import argparse
+import shlex
 import subprocess
 from primer_select.ps_configuration import PsConfigurationHandler
 import csv
@@ -20,9 +21,12 @@ for record in SeqIO.parse(handle, "fasta"):
     f.write("SEQUENCE_TEMPLATE=" + str(record.seq) + "\n")
     f.write("P3_FILE_FLAG=1\n")
     f.write("PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n=")
-    cmd = [config.p3_path, "-format_output", "-p3_settings_file=" + config.p3_config_path, "-output=" + record.id + "_p3.txt", p3file]
+
     #print cmd
-    p = subprocess.call(cmd)
+    cmd = config.p3_path + "-format_output -p3_settings_file=" + config.p3_config_path+ " -output=" + record.id + "_p3.txt "+ p3file
+    print cmd
+    args = shlex.split(cmd)
+    p = subprocess.call(args)
     #p.wait()
 handle.close()
 
