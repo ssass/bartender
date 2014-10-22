441f83f6d1bd5a05300a80c00cb4830751a9b052
ssass <steffen.sass@gmx.de>
2014-10-22 15:30:09 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -58,7 +58,7 @@ for record in SeqIO.parse(handle, "fasta"):
     primer_pairs = []
     for p3o in p3_output:
         p3_pairs = p3o.split("\t")
-        print(p3_pairs + "\n")
+        print str(p3_pairs) + "\n"
         #primer_pairs.append(PrimerPair(p3_pairs[0], p3_pairs[1]))
 
     primer_set.append(primer_pairs)
