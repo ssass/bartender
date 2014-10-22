4f06d1b10c5feee7860e24dc69203d6a4498b656
ssass <steffen.sass@gmx.de>
2014-10-22 13:48:02 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -14,6 +14,7 @@ ch = PsConfigurationHandler("config.cfg")
 config = ch.read_config()
 
 from Bio import SeqIO
+
 handle = open(args.input, "rU")
 for record in SeqIO.parse(handle, "fasta"):
     sequence = str(record.seq)
@@ -25,25 +26,25 @@ for record in SeqIO.parse(handle, "fasta"):
     target_start = sequence.find("\[")
     target_end = sequence.find("\]")
     target_length = target_end - target_start - 1
-    # print str(target_start) + "\n"
-    # print target_length
-    #
-    print "SEQUENCE_TARGET=" + str(target_start) + "," + str(target_length)
 
-  # exclude.start=regexpr("<",act.seq.ann,fixed=T)[1]
-  # exclude.length=regexpr(">",act.seq.ann,fixed=T)[1]-exclude.start-1
-  # if (exclude.start >(-1) & exclude.length > (-1))write(paste("SEQUENCE_EXCLUDED_REGION=",exclude.start,",",exclude.length,sep=""),file=paste("../p3_input/seq",i,".txt",sep=""),append=T)
-  #
+    if target_start >= 0 & target_length >= 0:
+        f.write("SEQUENCE_TARGET=" + str(target_start) + "," + str(target_length) + "\n")
 
+    exclude_start = sequence.find("<")
+    exclude_end = sequence.find(">")
+    exclude_length = exclude_end - exclude_start - 1
+
+    if target_start >= 0 & target_length >= 0:
+        f.write("SEQUENCE_EXCLUDED_REGION=" + str(exclude_start) + "," + str(exclude_length) + "\n")
 
     f.write("P3_FILE_FLAG=1\n")
     f.write("PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n=")
 
-    #print cmd
-    cmd = config.p3_path + " -format_output -p3_settings_file=" + config.p3_config_path + " -output=" + record.id + "_p3.txt "+ p3file
+    # print cmd
+    cmd = config.p3_path + " -format_output -p3_settings_file=" + config.p3_config_path + " -output=" + record.id + "_p3.txt " + p3file
     #print cmd
     args = shlex.split(cmd)
-    print args
+    # print args
     p = subprocess.Popen(args)
     #time.sleep(10)
 handle.close()
