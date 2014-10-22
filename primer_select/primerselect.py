edf860cb75495dcea6635c9b07e2f372d06c6168
ssass <steffen.sass@gmx.de>
2014-10-22 14:15:32 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -20,35 +20,35 @@ for record in SeqIO.parse(handle, "fasta"):
     sequence = str(record.seq)
     p3file = record.id + "_seq.txt"
 
-    input_String = ""
+    input_string = ""
     #f = open(p3file, 'w')
-    input_String += "SEQUENCE_ID=" + record.id + "\n"
-    input_String += "SEQUENCE_TEMPLATE=" + sequence + "\n"
+    input_string += "SEQUENCE_ID=" + record.id + "\n"
+    input_string += "SEQUENCE_TEMPLATE=" + sequence + "\n"
 
-    target_start = sequence.find("\[")
-    target_end = sequence.find("\]")
+    target_start = sequence.find("[")
+    target_end = sequence.find("]")
     target_length = target_end - target_start - 1
+    sequence.replace("[", "")
+    sequence.replace("]", "")
 
     if target_start >= 0 & target_length >= 0:
-        input_String += "SEQUENCE_TARGET=" + str(target_start) + "," + str(target_length) + "\n"
+        input_string += "SEQUENCE_TARGET=" + str(target_start) + "," + str(target_length) + "\n"
 
     exclude_start = sequence.find("<")
     exclude_end = sequence.find(">")
     exclude_length = exclude_end - exclude_start - 1
 
     if target_start >= 0 & target_length >= 0:
-        input_String += "SEQUENCE_EXCLUDED_REGION=" + str(exclude_start) + "," + str(exclude_length) + "\n"
+        input_string += "SEQUENCE_EXCLUDED_REGION=" + str(exclude_start) + "," + str(exclude_length) + "\n"
 
-    input_String += "P3_FILE_FLAG=1\n"
-    input_String += "PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n="
+    input_string += "P3_FILE_FLAG=1\n"
+    input_string += "PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n="
 
-    # print cmd
+    print input_string + "\n"
+
     cmd = config.p3_path + " -format_output -p3_settings_file=" + config.p3_config_path + " -output=" + record.id + "_p3.txt"
-    print cmd
     args = shlex.split(cmd)
-    # print args
     p = subprocess.Popen(args, stdin=subprocess.PIPE)
-    p.communicate(input_String)
+    p.communicate(input_string)
     p.wait()
 handle.close()
-#time.sleep(5)
