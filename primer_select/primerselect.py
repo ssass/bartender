355f28f3acba2392caa224fff9a65b6d0c16f6c1
ssass <steffen.sass@gmx.de>
2014-10-22 11:18:40 +0200

    minor changes

--- primer_select/primerselect.py
+++ primer_select/primerselect.py
@@ -20,7 +20,7 @@ for record in SeqIO.parse(handle, "fasta"):
     f.write("SEQUENCE_TEMPLATE=" + str(record.seq) + "\n")
     f.write("P3_FILE_FLAG=1\n")
     f.write("PRIMER_THERMODYNAMIC_PARAMETERS_PATH=" + config.p3_thermo_path + "\n=")
-    cmd = [config.p3_path, "-format_output", "-p3_settings_file=" + config.p3_config_path, "-output=" + record.id + "_p3.txt", p3file]
+    cmd = [config.p3_path, "-p3_settings_file=" + config.p3_config_path, "-output=" + record.id + "_p3.txt", p3file]
     print cmd
     p = subprocess.Popen(cmd)
     #p.wait()
