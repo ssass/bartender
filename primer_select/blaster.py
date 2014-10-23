e7874ae364e7a5779fe8310c562af61baca723b8
ssass <steffen.sass@gmx.de>
2014-10-23 17:15:56 +0200

    minor changes

--- primer_select/blaster.py
+++ primer_select/blaster.py
@@ -12,24 +12,24 @@ class Blaster:
 
     def run_process(self, primer_set, args):
         print("Blasting primer", primer_set.name, "...")
-            blast_string = ""
-            for pair in primer_set.set:
-                blast_string += ">" + pair.name + "_fwd\n" + pair.fwd + "\n\n"
-                blast_string += ">" + pair.name + "_rev\n" + pair.rev + "\n\n"
+        blast_string = ""
+        for pair in primer_set.set:
+            blast_string += ">" + pair.name + "_fwd\n" + pair.fwd + "\n\n"
+            blast_string += ">" + pair.name + "_rev\n" + pair.rev + "\n\n"
 
-            p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            blast_output = p.communicate(blast_string)[0].strip()
-            # print(blast_output)
-            blast_output = blast_output.split("\n")
-            blast_hits = []
-            for line in blast_output:
-                act_result = line.strip().split("\t")
-                if float(act_result[10]) < 0.1:
-                    blast_hits.append(act_result[0])
+        p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        blast_output = p.communicate(blast_string)[0].strip()
+        # print(blast_output)
+        blast_output = blast_output.split("\n")
+        blast_hits = []
+        for line in blast_output:
+            act_result = line.strip().split("\t")
+            if float(act_result[10]) < 0.1:
+                blast_hits.append(act_result[0])
 
-            for pair in primer_set.set:
-                pair.blast_hits[0] = blast_hits.count(pair.name + "_fwd")
-                pair.blast_hits[1] = blast_hits.count(pair.name + "_rev")
+        for pair in primer_set.set:
+            pair.blast_hits[0] = blast_hits.count(pair.name + "_fwd")
+            pair.blast_hits[1] = blast_hits.count(pair.name + "_rev")
 
 
     def blast_primer_set(self, primer_sets):
