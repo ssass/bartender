3ddbbbf946372f80e5e2dfe485e414a80402e29d
ssass <steffen.sass@gmx.de>
2014-11-03 13:30:30 +0100

    minor changes

--- web_frontend/start_service.py
+++ web_frontend/start_service.py
@@ -32,7 +32,7 @@ def submit():
         primer_sets = PrimerSelect.predict_primerset(input_handle=input, predefined_handle=None, config=config)
         opt_result = PrimerSelect.optimize(config, primer_sets)
         output = PrimerSelect.output(opt_result, primer_sets)
-        print(output)
+        return render_template('base.html', form=form, output=output)
     return render_template('base.html', form=form)
 
 
